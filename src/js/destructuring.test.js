b80f53051bc2989a85b6622c578a5853b767ff0c
artemkosnikov <Boxberry123>
2021-06-02 15:37:01 +0300

    init commit

---
+++ src/js/destructuring.test.js
@@ -0,0 +1,34 @@
+import destructuring from './destructuring';
+
+test('Проверка, что функция возвращает массив верных объектов', () => {
+  const result = destructuring({
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      // <- обратите внимание, описание "засекречено"
+      },
+    ],
+  });
+  expect(result).toEqual([
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ]);
+});
